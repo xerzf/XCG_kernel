45e517429cefbeadd256b542423533001d02bb1b
xrf0220 <xrfgooo@gmail.com>
2024-02-29 02:54:12 +0000

    aa

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -5991,6 +5991,7 @@ static void
 cgroup_async_create_fn(struct cgroup_subsys_state* css) {
 	int ret = 0;
 	struct cgroup* cgrp = css->cgroup;
+	printk("cgroup address %d\n", cgrp);
 	ret = percpu_ref_init(&cgrp->self.refcnt, css_release, 0, GFP_KERNEL);
 	if (ret)
 		panic("percpu_ref_init fail.\n");
